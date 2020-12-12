fbc82ac4172d77a09ddef00b4fc5ce0792d97956
l3sombre <karinhizhnyak@gmail.com>
2020-12-12 16:01:15 +0200

    Adding conftest.py file, environment requirements, file with first test and empty __init__.py

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action="store", default="en", help="Choose a language of your tests: ")
+   
+
+@pytest.fixture(scope="function")
+def browser(request):
+    test_language = request.config.getoption("language")
+    if (test_language):
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages':test_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        print ("Please, choose a language of the site. You have to add parameter '--language lang_code' before starting a test.")
+    yield browser
+    browser.quit()
+
