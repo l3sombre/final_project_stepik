7fd3636f0479385a94628d6f66f3ffe241523830
l3sombre <karinhizhnyak@gmail.com>
2020-12-14 17:26:56 +0200

    Rewriting test in test_main_page.py with using page objects

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
-link = "http://selenium1py.pythonanywhere.com/"
+#import sys
+#sys.path.append("C:\\Users\\karin\\Desktop\\pypi_and_selenium\\final_project_stepik\\pages")
+
+#from final_project_stepik.pages.main_page import MainPage
+from .pages.main_page import MainPage
+
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser) 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/" 
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
