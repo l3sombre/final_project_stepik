7c56dccc5c2c38edd84aa5ea8cc837273b198c13
l3sombre <karinhizhnyak@gmail.com>
2020-12-14 17:45:04 +0200

    Adding implicit waiting and small exception handler for  non-displayable elements; changing test with this handler

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,4 @@
-#import sys
-#sys.path.append("C:\\Users\\karin\\Desktop\\pypi_and_selenium\\final_project_stepik\\pages")
 
-#from final_project_stepik.pages.main_page import MainPage
 from .pages.main_page import MainPage
 
 def go_to_login_page(browser):
@@ -13,3 +10,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
