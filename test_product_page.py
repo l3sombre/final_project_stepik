c78cd08cb44fde1b295f6fe6c573699fe857f8f0
l3sombre <69056428+l3sombre@users.noreply.github.com>
2021-01-17 18:14:04 +0200

    Adding missing parameter to the list of promo offers.
    I've forgotten about "2" in the list of promo offers, so I add it back quickly.

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,7 @@ import pytest
 import faker
 
 
-@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", \
+@pytest.mark.parametrize('promo_offer', ["0","1", "2", "3", "4", "5", "6", \
     pytest.param("7", marks=pytest.mark.xfail(reason="bugged link!")), "8", "9"])
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, promo_offer):
